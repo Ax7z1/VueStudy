b02391d638b5643942a35e48fa6775bf5a3e50b2
Ax7z1 <Ax7z1@foxmail.com>
2023-12-20 16:04:48 +0800

    Vue框架数据代理实现与源代码解读

--- js/myvue.js
+++ js/myvue.js
@@ -3,18 +3,21 @@ class Vue {
     // 定义构造函数
     // options是一个简单的纯粹的JS对象
     // options对象中有一个data配置项
-    constructor(options){
+    constructor(options) {
         //  获取所有的属性名
         Object.keys(options.data).forEach((propertyName, index) => {
-           // console.log(typeof propertyName, index)
-           Object.defineProperty(this, propertyName, {
-                get(){
-                    return options.data[propertyName]
-                },
-                set(val){
-                    options.data[propertyName] = val
-                }
-           })
+            // console.log(typeof propertyName, index)
+            let firstChar = propertyName.charAt(0)
+            if (firstChar != '_' && firstChar != '$') {
+                Object.defineProperty(this, propertyName, {
+                    get() {
+                        return options.data[propertyName]
+                    },
+                    set(val) {
+                        options.data[propertyName] = val
+                    }
+                })
+            }
         })
     }
 }
