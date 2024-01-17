d28eb2eb81f612461e3fcc95f612557277de67b6
Ax7z1 <Ax7z1@foxmail.com>
2024-01-17 17:53:55 +0800

    响应式与数据劫持

--- js/myvue.js
+++ js/myvue.js
@@ -10,20 +10,25 @@ class Vue {
             let firstChar = propertyName.charAt(0)
             if (firstChar != '_' && firstChar != '$') {
                 Object.defineProperty(this, propertyName, {
+                    // 数据代理
                     get() {
                         return options.data[propertyName]
                     },
+                    // 数据劫持
                     set(val) {
+                        // 1.修改内存中该对象的属性值
                         options.data[propertyName] = val
+                        // 2.重新渲染页面
+                        console.log('页面上的真实DOM元素更新了！！！');
                     }
                 })
             }
         })
-        // 获取所有的方法名
+      /*   // 获取所有的方法名
         Object.keys(options.methods).forEach((methodName, index) => 
         {
             // 给当前的Vue实例扩展一个方法
             this[methodName] = options.methods[methodName]
-        })
+        }) */
     }
 }
